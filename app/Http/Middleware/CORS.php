90898dd2db99a71138cb96a299ecf5eb340d5569
aziz <medaziz.cherif00@gmail.com>
2021-03-12 16:57:43 +0100

    fourth_commit

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -16,8 +16,11 @@ class CORS
      */
     public function handle(Request $request, Closure $next)
     {
-        header('Acces-Controll-Allow-Origin : *');
-        header('Acces-Controll-Allow-Origin : Content-Type ,X-Auth-Token , Authorisation , Origin');
+        header('Access-Control-Allow-Origin', '*');
+        header('Access-Control-Allow-Methods', '*');
+        header('Access-Control-Allow-Credentials', true);
+        header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization');
+        header('Accept', 'application/json');
         return $next($request);
     }
 }
